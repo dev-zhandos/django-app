bbfbfb88ad06488b51c6db6a986d43160909c63f
Zhandos Usen <zusen@gratanet.com>
2023-02-23 16:35:20 +0600

    Update app

--- app/django_sample/views.py
+++ app/django_sample/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the index.")
+    return HttpResponse("Hello, ArgoCD. You're at the index.")
